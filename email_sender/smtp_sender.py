f3ab760e9a6f86bcbf008d8029692262658bad4d
8804 <8804who@naver.com>
2026-02-12 23:24:18 +0900

    add(smtp): add function(markdown to html)

--- email_sender/smtp_sender.py
+++ email_sender/smtp_sender.py
@@ -4,6 +4,8 @@ from email.mime.text import MIMEText
 import base64
 import smtplib
 
+from markdown_it import MarkdownIt
+
 from config import SMTP
 from email_sender.email_sender import EmailSender
 
@@ -21,12 +23,18 @@ class SMTPSender(EmailSender):
         message['From'] = self.smtp_username
         message['To'] = to
         message['Subject'] = subject
-        message.attach(MIMEText(body, 'plain'))
+        html_body = self.convert_markdown_to_html(body)
+        message.attach(MIMEText(html_body, 'html'))
         text = message.as_string()
         server = smtplib.SMTP(self.smtp_server, self.smtp_port)
         server.starttls()
         server.login(self.smtp_username, self.smtp_password)
         server.sendmail(self.smtp_username, to, text)
         server.quit()
+
+    def convert_markdown_to_html(self, markdown_text: str):
+        md = MarkdownIt("commonmark", {"html": True}).enable("table")
+        html_body = md.render(markdown_text)
+        return html_body
 
 smtp_sender = SMTPSender()
