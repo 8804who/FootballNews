d0e96a0e9e76e9b9aed3ee89a6d4d8d49c9f5233
8804 <8804who@naver.com>
2026-03-19 23:33:14 +0900

    revise(config): use env if api.yaml not exist

--- config.py
+++ config.py
@@ -1,12 +1,30 @@
+import os
 from yaml import load, Loader
 
 ### API KEYS, URLS ###
-with open("api.yml", "r") as f:
-    data = load(f, Loader=Loader)
-    API_KEY = data["API_KEY"]
-    URL = data["URL"]
-    GOOGLE_CLOUD = data["GOOGLE_CLOUD"]
-    SMTP = data["SMTP"]
+if os.path.exists("api.yml"):
+    with open("api.yml", "r") as f:
+        data = load(f, Loader=Loader)
+        API_KEY = data["API_KEY"]
+        URL = data["URL"]
+        GOOGLE_CLOUD = data["GOOGLE_CLOUD"]
+        SMTP = data["SMTP"]
+else:
+    API_KEY = {"OPENAI": os.environ["OPENAI_API_KEY"]}
+    URL = {
+        "FPL": os.environ.get("URL_FPL", "https://fantasy.premierleague.com/api/"),
+        "fbref": os.environ.get("URL_FBREF", "https://fbref.com/"),
+    }
+    GOOGLE_CLOUD = {
+        "EMAIL": os.environ["GOOGLE_CLOUD_EMAIL"],
+        "SPREADSHEET_URL": os.environ["GOOGLE_CLOUD_SPREADSHEET_URL"],
+    }
+    SMTP = {
+        "SERVER": os.environ.get("SMTP_SERVER", "smtp.gmail.com"),
+        "PORT": int(os.environ.get("SMTP_PORT", "587")),
+        "USERNAME": os.environ["SMTP_USERNAME"],
+        "PASSWORD": os.environ["SMTP_PASSWORD"],
+    }
 
 ### EXAMPLE ###
 with open("example.yml", "r") as f:
